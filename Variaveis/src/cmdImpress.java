d9a1a3a7ae0ca71978a5d62920389e46c057ca80
Samuel <samukactto@gmail.com>
2023-11-12 03:30:09 -0300

    comando de impressão e variaveis

---
+++ Variaveis/src/cmdImpress.java
@@ -0,0 +1,17 @@
+public class cmdImpress {
+    public static void main(String[] args) throws Exception {
+
+        //VARIAVEIS
+        int n1 = 2; 
+        int n2 = 20;
+        int n3 = 5;
+        int res = n1 + n2 + n3;
+        String nome = "Samuel";
+
+        //COMANDOS DE IMPRESSÃO
+        System.out.print("Samukinha\n");
+        System.out.println("Curso Java");
+        System.out.printf("Canal:%s%nAno:%d", "CFB Cursos", 2021);
+        System.out.printf("Valor da variavel num: %d%nNome:%s", res,nome);
+    }
+}
